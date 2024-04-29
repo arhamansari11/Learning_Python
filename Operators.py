1867a6369be341d35754f59b38b39430cce36b20
Arham <arhamansari454647@gmail.com>
2024-04-29 20:02:35 +0500

    Operators in Python

---
+++ Operators.py
@@ -0,0 +1,58 @@
+# Operators
+
+# There are 4 types of Operators.
+
+# 1-> Arithemetic Operators.
+
+# a = 12
+# b = 2
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a ** b)
+# print(a % b)
+
+
+
+# 2-> Assignment Operators
+
+a = 6
+b = 3
+# a += b    
+# print(a)
+# a -= b
+# print(a)
+# a *= b
+# print(a)
+# a /= b
+# print(a)
+# a //= b
+# print(a)
+# a **= b
+# print(a)
+# a %= b
+# print(a)
+
+
+# 3-> Comparison Operators
+
+# a = 18
+# b = 9
+
+# print(a > b)
+# print(a < b)
+# print(a == b)
+
+
+# 4-> Logical Operators
+
+# a = 8
+# b = 23
+# c = 43
+
+# print(a > b and a > c)
+# print (b > c or c > b)
+# print(not(True))
+# print(not(False))
