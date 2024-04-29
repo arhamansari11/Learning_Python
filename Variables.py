4baffa037aefb905d0f0b802aae42d0eec6e0b05
Arham <arhamansari454647@gmail.com>
2024-04-29 18:00:03 +0500

    Variables , Comments , TypeCasting

---
+++ Variables.py
@@ -0,0 +1,37 @@
+#    -> We use Print function to show something in input.
+#  print("Hello World!")
+
+
+# Comments in Python
+
+# -> Single Line Comment
+'''
+    Multi-Line Comments
+'''
+
+# Variables in Python
+
+a = 12
+b = 6
+print(a + b)
+
+a = "Arham"
+b = "Ansari"
+print(a , b)
+
+a = True
+b = False
+print(a , b)
+
+a = None
+print(a)
+
+
+
+# TypeCasting in Python
+
+new = 12
+print(str(new) , "Done")
+
+test  = "12"
+print(int(test) + 1)
--- main.py
+++
@@ -1,1 +0,0 @@
-print("Just Start Python")
