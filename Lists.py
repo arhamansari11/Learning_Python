027973885be55d144d3d7889f1fefc8bc8b7448a
Arham <arhamansari454647@gmail.com>
2024-04-30 20:12:55 +0500

    List and Lists Methods

---
+++ Lists.py
@@ -0,0 +1,65 @@
+list1 = []
+
+# 1-> Sort
+# Sorts the list
+# list1.sort()
+# print(list1)
+
+
+# 2-> Reverse
+# Reverse the List
+# list1.reverse()
+# print(list1)
+
+
+# 3-> Remove
+# Removes the first item with the specified value
+# list1.remove(3)
+# print(list1)
+
+# 4-> Pop
+# Remove the Selected Element . If not selected delete the last list
+# list1.pop(3)
+# print(list1)
+
+
+# 5-> Insert
+# Insert the Selected Element . If not selected insert at last list
+# list1.insert(1 , 12)
+# print(list1)
+
+
+# 6-> Index
+# Returns the index of the first element with the specified value
+# x=list1.index(3)
+# print(x)
+
+
+# 7-> Extend
+# Add elements in the last of the Array
+# extlist = [11 , 343 , 43]
+# list1.extend(extlist)
+# print(list1)
+
+
+# 8-> Count
+# Returns the number of elements with the specified value
+# x = list1.count(1)
+# print(x)
+
+
+# 9-> Copy
+# Returns the copy of the Array
+# x = list1.copy()
+# print(x)
+
+
+# 10-> Clear
+# Clear all the Array
+# print(list1.clear())
+
+
+# 11-> Append
+# The append() method appends an element to the end of the list.
+# list1.append(18)
+# print(list1)
