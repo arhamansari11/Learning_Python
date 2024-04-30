0003fc7692c5eebd2d14be00f654e8db84db33bd
Arham <arhamansari454647@gmail.com>
2024-04-30 15:53:29 +0500

    String and String Methods

---
+++ Strings.py
@@ -0,0 +1,65 @@
+name = "hello, i am muhammad arham"
+
+# 1->  Split Method
+# Split the Strings into the Sub Strings    
+# print(name.split())
+
+
+# name = "The price of this product is {price}"
+
+# 2-> Format Method
+#  The format() method formats the specified value(s) and insert them inside the string's placeholder.
+# print(name.format(price = "Fifty Rupees"))
+
+
+# 3-> Replace
+# Replaces the  words in the string with the other one.
+
+# print(name.replace("Nadeem" , "Hameed"))
+
+
+# 4-> Count
+# Count how many words are their in String
+# print(name.count("Arham"))
+
+
+# 5-> Center
+# Centered the String where you want
+# print(name.center(20))
+
+
+# 6-> Join
+# Join Character or Special Characters in String
+# x = " and ".join(name)
+# print(x)    
+
+# 7-> Find
+# Find the character in the String
+# print(name.find("muhammad"))
+
+# 8-> Capitilize
+# Capitilize the 1st character of the elements in the String
+# print(name.capitalize())
+
+# 9-> StartsWith
+# Returns true if the string starts with the specified value
+# print(name.startswith("hello"))
+
+# 10-> Title
+# Converts the first character of each word to upper case
+# print(name.title())
+
+# 11-> Upper
+# Converts a string into upper case
+# print(name.upper())
+
+
+# 12-> Strip
+# Remove empty Spaces
+# print(name.strip())
+
+
+# 13-> Index
+# Returns the index in the name
+# x = name.title()
+# print(x.index("I"))
