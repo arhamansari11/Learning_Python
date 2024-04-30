672e33b0339b37e6ef018904d92fba0459d9579a
Arham <arhamansari454647@gmail.com>
2024-04-30 18:25:26 +0500

    Input in Python

---
+++ Input.py
@@ -0,0 +1,10 @@
+name = input("Enter your Name : ")
+print("Your name is " + name)
+
+
+# Input tag only take input in Integer form
+
+number = input("Enter the Number : ")
+print("You enter :" , (int(number) + 6) )
+
+
