93d8fbfac3720d9f9b91c164eeb0f8f348926712
Arham <arhamansari454647@gmail.com>
2024-05-01 17:16:14 +0500

    If-Else Statement in Python

--- Ifelse.py
+++ Ifelse.py
@@ -1,7 +1,16 @@
 gender = input("Enter your Gender : ")
-if gender == "male":
-    print("You are male")
-elif gender == "female":
-    print("You are female")
+
+if gender == "Male":
+    salary = int(input("Enter your salary : "))
+    if salary >= 30000:
+        print("You are eligible for loan")
+    else:
+        print("You are not Eligible for loan")
+elif gender == "Female":
+    salary = int(input("Enter your Salary : "))
+    if salary >= 20000:
+        print("You are Eligible for loan")
+    else:
+        print("You are not Eligible for loan")
 else:
-    print("Check your gender")
+    print("Check your Gender Please")
