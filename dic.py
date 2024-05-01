93f920308d329edc2c92e86d61d42b751170f15f
Arham <arhamansari454647@gmail.com>
2024-05-01 16:26:00 +0500

    Dictionaries in Python

---
+++ dic.py
@@ -0,0 +1,32 @@
+age = {"Arham" : 18 , "Aiza" : 22 , "Gulshair" : 21 , "Abu Bakar" : 20}
+
+# 1->  Get Methods return the value of selected key
+# print(age.get("Arham"))
+
+# 2-> Key method returns all the keys
+# print(age.keys())
+
+# 3-> Values method returns all the values
+# print(age.values())
+
+# 4-> Items method returns all the dict elements in Tupples
+# print(age.items())
+
+# 5-> Pop Removes the element with the specified key
+# age.pop("Arham")
+# print(age)
+
+
+# 6-> Pop Item Removes the element from end
+# age.popitem()
+# print(age)
+
+# 7-> Set Default 
+# x = age.setdefault("Ahmad" , 17)
+# print(age)
+
+# 8-> Update
+# x = age.update({"Arham" : 909})
+# print(age)
+
+
