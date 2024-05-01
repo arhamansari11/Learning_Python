fa52767dcd4affc47ede671bcaeb7cc8403fb3d2
Arham <arhamansari454647@gmail.com>
2024-05-01 16:01:53 +0500

    Sets in Python

---
+++ Sets.py
@@ -0,0 +1,15 @@
+set1 = {1 ,789 , 7671 ,2}
+set2 = {1 ,2 ,3 ,4}
+
+# print(len(set1))
+
+# Union  and Intersection
+# x = set1.intersection(set2)
+# x1 = set1.union(set2)
+# print(x)
+
+
+
+
+# set2.pop()
+# print(set2)
