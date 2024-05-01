6f38642beaaca02f7c660963e828ed651b6dc582
Arham <arhamansari454647@gmail.com>
2024-05-01 19:42:30 +0500

    For-Loop in Python

---
+++ For_Loop.py
@@ -0,0 +1,25 @@
+# Loop Practice in List
+
+# list1 = [ 1 ,2 ,3 ,4 ,5 ,6]
+# sum = 0
+# length = len(list1)
+# ans = []
+# for i in range(length):
+#     sum += i + 1
+#     ans.append(sum)
+
+# print(ans)
+
+
+
+# Table using Loop
+
+
+
+# table = int(input("Enter your Table number : "))
+
+# sum = 1
+
+# for i in range(10):
+#     mult = table * (i + 1)
+#     print(mult)
