f5260cabc58114f45959ef085c4e98d70fb311c9
Arham <arhamansari454647@gmail.com>
2024-05-02 10:24:39 +0500

    Functions in Python

---
+++ Function.py
@@ -0,0 +1,24 @@
+# def greetHello(name , ending):
+#     for i in range(5):
+#         print("Hello " + name + ending)
+
+# greetHello("Arham" , " Done")
+
+
+
+
+# def students(name , date):
+#     st = f"Hello \nThis is {name} and I attend your session on {date}"
+#     print(st)
+
+
+# students("Arham" , "2 May")
+# students("Ali" , "2 May")
+# students("Ahmad" , "2 May")
+
+
+# def stringdef(classs):
+#     return(f"I am in {classs}")
+
+# print(stringdef("12th"))
+
