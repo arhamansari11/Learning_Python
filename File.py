b4a67483bde530c2d14de4775e2e04a1e30fb477
Arham <arhamansari454647@gmail.com>
2024-05-02 16:56:45 +0500

    File I/O in Python

---
+++ File.py
@@ -0,0 +1,22 @@
+s = "Arham is a Elegent Boy"
+
+
+# 1-> Wrting to a File
+
+# with open("text.txt" , "w") as f:
+#     f.write(s)  
+
+
+
+# 2-> Reading a File
+
+
+# with open("text.txt" , "r") as f:
+#     s = f.read()
+#     print(s)
+
+
+#  3-> Appending in a File
+
+# with open("text.txt" , "a") as f:
+#     f.write(s)
